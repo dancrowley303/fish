d5509b1d03b0af55996f133bd57ec00a189436e5
Daniel Crowley-Wilson <daniel.crowley.wilson@gmail.com>
2015-03-01 15:51:36 +0900

    don't need to redefine "new" with Visual Leak Detector

--- fish.cpp
+++ fish.cpp
@@ -7,13 +7,6 @@
 
 #include "vld.h"
 
-#ifdef _DEBUG
-   #ifndef DBG_NEW
-      #define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
-      #define new DBG_NEW
-   #endif
-#endif  // _DEBUG
-
 int _tmain(int argc, _TCHAR* argv[])
 {
 	std::srand((unsigned int)std::time(0));
